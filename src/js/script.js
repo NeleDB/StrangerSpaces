f8bfacb263b591e770ce3f1a8891c88d49ecaef3
Lotte Bijlsma <bijlsmalotte@gmail.com>
2017-12-06 23:05:11 +0100

    tried to fix await and async

--- src/js/script.js
+++ src/js/script.js
@@ -19,51 +19,51 @@ const STYLE_MAPPINGS = {
 const STYLE_NAMES = Object.keys(STYLE_MAPPINGS);
 
 
-const __extends = (this && this.__extends) || (function () {
-  const extendStatics = Object.setPrototypeOf ||
-        ({__proto__: []} instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (const p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-  return function (d, b) {
-    extendStatics(d, b);
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-  };
-})();
-const __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-  return new (P || (P = Promise))(function (resolve, reject) {
-    function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-    function rejected(value) { try { step(generator[`throw`](value)); } catch (e) { reject(e); } }
-    function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-    step((generator = generator.apply(thisArg, _arguments || [])).next());
-  });
-};
-const __generator = (this && this.__generator) || function (thisArg, body) {
-  let _ = {label: 0, sent: function() { if (t[0] & 1) throw t[1];return t[1]; }, trys: [], ops: []}, f, y, t, g;
-  return g = {next: verb(0), throw: verb(1), return: verb(2)}, typeof Symbol === `function` && (g[Symbol.iterator] = function() { return this; }), g;
-  function verb(n) { return function (v) { return step([n, v]); }; }
-  function step(op) {
-    if (f) throw new TypeError(`Generator is already executing.`);
-    while (_) try {
-      if (f = 1, y && (t = y[op[0] & 2 ? `return` : op[0] ? `throw` : `next`]) && !(t = t.call(y, op[1])).done) return t;
-      if (y = 0, t) op = [0, t.value];
-      switch (op[0]) {
-      case 0: case 1: t = op;break;
-      case 4: _.label++;return {value: op[1], done: false};
-      case 5: _.label++;y = op[1];op = [0];continue;
-      case 7: op = _.ops.pop();_.trys.pop();continue;
-      default:
-        if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0;continue; }
-        if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1];break; }
-        if (op[0] === 6 && _.label < t[1]) { _.label = t[1];t = op;break; }
-        if (t && _.label < t[2]) { _.label = t[2];_.ops.push(op);break; }
-        if (t[2]) _.ops.pop();
-        _.trys.pop();continue;
-      }
-      op = body.call(thisArg, _);
-    } catch (e) { op = [6, e];y = 0; } finally { f = t = 0; }
-    if (op[0] & 5) throw op[1];return {value: op[0] ? op[1] : void 0, done: true};
-  }
-};
+// const __extends = (this && this.__extends) || (function () {
+//   const extendStatics = Object.setPrototypeOf ||
+//         ({__proto__: []} instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+//         function (d, b) { for (const p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+//   return function (d, b) {
+//     extendStatics(d, b);
+//     function __() { this.constructor = d; }
+//     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+//   };
+// })();
+// const __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+//   return new (P || (P = Promise))(function (resolve, reject) {
+//     function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+//     function rejected(value) { try { step(generator[`throw`](value)); } catch (e) { reject(e); } }
+//     function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+//     step((generator = generator.apply(thisArg, _arguments || [])).next());
+//   });
+// };
+// const __generator = (this && this.__generator) || function (thisArg, body) {
+//   let _ = {label: 0, sent: function() { if (t[0] & 1) throw t[1];return t[1]; }, trys: [], ops: []}, f, y, t, g;
+//   return g = {next: verb(0), throw: verb(1), return: verb(2)}, typeof Symbol === `function` && (g[Symbol.iterator] = function() { return this; }), g;
+//   function verb(n) { return function (v) { return step([n, v]); }; }
+//   function step(op) {
+//     if (f) throw new TypeError(`Generator is already executing.`);
+//     while (_) try {
+//       if (f = 1, y && (t = y[op[0] & 2 ? `return` : op[0] ? `throw` : `next`]) && !(t = t.call(y, op[1])).done) return t;
+//       if (y = 0, t) op = [0, t.value];
+//       switch (op[0]) {
+//       case 0: case 1: t = op;break;
+//       case 4: _.label++;return {value: op[1], done: false};
+//       case 5: _.label++;y = op[1];op = [0];continue;
+//       case 7: op = _.ops.pop();_.trys.pop();continue;
+//       default:
+//         if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0;continue; }
+//         if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1];break; }
+//         if (op[0] === 6 && _.label < t[1]) { _.label = t[1];t = op;break; }
+//         if (t && _.label < t[2]) { _.label = t[2];_.ops.push(op);break; }
+//         if (t[2]) _.ops.pop();
+//         _.trys.pop();continue;
+//       }
+//       op = body.call(thisArg, _);
+//     } catch (e) { op = [6, e];y = 0; } finally { f = t = 0; }
+//     if (op[0] & 5) throw op[1];return {value: op[0] ? op[1] : void 0, done: true};
+//   }
+// };
 
 const init = () => {
   const canvas = document.querySelector(`.imageCanvas`);
@@ -121,7 +121,7 @@ const init = () => {
     transformNet.load().then(
       () => {
         startButton.innerText = `Processing image`;
-        runInference();
+        //runInference();
         startButton.innerText = `Start style transfer button`;
         startButton.disabled = false;
       }
@@ -135,18 +135,20 @@ const init = () => {
     });
   });
 
-  const runInference = () =>{
+  async () => {
+    //hier ergens nog runInference(); declareren!!!!
     await math.scope(async (keep, track) => {
-      const preprocessed = track(Array3D.fromPixels(contentImgElement));
-      const inferenceResult = await transformNet.predict(preprocessed);
+      const preprocessed = track(Array3D.fromPixels(this.contentImgElement));
 
+      const inferenceResult = await transformNet.predict(preprocessed);
       setCanvasShape(inferenceResult.shape);
-      const renderShader = render_ndarray_gpu_util.getRenderRGBShader(//eslint-disable-line
-        gpgpu, inferenceResult.shape[1]);
+      const renderShader = render_ndarray_gpu_util.getRenderRGBShader( //eslint-disable-line
+          this.gpgpu, inferenceResult.shape[1]);
       render_ndarray_gpu_util.renderToCanvas( //eslint-disable-line
-        gpgpu, renderShader, inferenceResult.getTexture());
+          gpgpu, renderShader, inferenceResult.getTexture());
     });
   };
+
 
   const setCanvasShape = shape => {
     console.log(shape);
